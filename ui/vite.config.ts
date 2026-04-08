59c288045e0480d91abb3b11f9999fd41a8bab4f
Astronaut <85165800+lAstronautl@users.noreply.github.com>
2026-04-08 10:55:10 +0200

    update deployment

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,6 +5,7 @@ import postcssNested from 'postcss-nested';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: process.env.GITHUB_PAGES ? '/starhorizon-map-render/' : '/',
   css: {
     postcss: {
       plugins: [postcssNested()],
